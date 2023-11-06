3b2e313f13dae9d4587bce5fac0d1ef33571f8ba
Mohammad Hasan Pourbakhsh <mh.pourbakhsh@gmail.com>
2023-11-06 22:29:26 +0330

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
